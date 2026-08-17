d8b74c72edd0fc37189350191f68859fd2ca3b2d
nhienovio-alt <nhienovio@gmail.com>
2026-08-17 10:22:08 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const { joinVoiceChannel } = require('@discordjs/voice');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates]
+});
+
+client.on('ready', () => {
+  console.log(`Bot ${client.user.tag} đã online!`);
+  
+  joinVoiceChannel({
+    channelId: '1538121272700051456',
+    guildId: '1537062273569394689',
+    adapterCreator: client.guilds.cache.get('1537062273569394689').voiceAdapterCreator,
+    selfDeaf: true,
+  });
+});
+
+client.login('MTUzODczODE4MDIxODQyNTM5OA.Gom0PL.pxqpG0lI1y6JTwPod5pLnxmryK2kLfF4jw9mMU');
