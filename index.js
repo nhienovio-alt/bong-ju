3b24b697c972fdf9bfaa4c964c2b68897427ac48
nhienovio-alt <nhienovio@gmail.com>
2026-08-17 10:43:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -17,3 +17,12 @@ client.on('ready', () => {
 });
 
 client.login('MTUzODczODE4MDIxODQyNTM5OA.Gom0PL.pxqpG0lI1y6JTwPod5pLnxmryK2kLfF4jw9mMU');
+// Thêm đoạn này để catch lỗi login
+client.login(process.env.DISCORD_TOKEN).catch(error => {
+  console.error("Lỗi đăng nhập Discord Bot:", error);
+});
+
+// Thêm catch lỗi toàn cục để tránh sập app đột ngột
+process.on('unhandledRejection', error => {
+  console.error('Unhandled promise rejection:', error);
+});
